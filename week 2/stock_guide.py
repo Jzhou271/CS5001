5fa29690e41a1851d1edbb78216df2e6a51cd3a1
Jzhou271 <98428764+Jzhou271@users.noreply.github.com>
2022-02-24 14:13:51 -0800

    Update stock_guide.py

--- week 2/stock_guide.py
+++ week 2/stock_guide.py
@@ -1,8 +1,14 @@
-'''CS5001
-   Lab 2
-   Question 4
-   Jing Zhou
-   1/31/3022
+'''
+You are looking to invest money in some new stocks. But you need help 
+deciding if you should buy a little, buy a lot, sell, or stay. Create 
+a system that takes in a number representing an analyst rating. If 
+the rating is above 100 the program should output "Buy a lot", if 
+the rating is above 76 and no higher than 100 the system should 
+output "Buy a little", if the rating is between 50 and 76 the output
+should be "Stay", if the rating is lower than 50 but above 25 the 
+output should be "Sell", and if the rating is 25 or less the output 
+should be "Sell! Sell! Sell!"
+1/31/3022
 '''
 
 
