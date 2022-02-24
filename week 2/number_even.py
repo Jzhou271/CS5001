39701cdafd99a040be4abcdf5e180d41986daa08
Jzhou271 <98428764+Jzhou271@users.noreply.github.com>
2022-02-24 14:12:23 -0800

    Update number_even.py

--- week 2/number_even.py
+++ week 2/number_even.py
@@ -1,5 +1,7 @@
 '''
-program should have the user input a single integer value, and then should print out if that is an even or odd number.
+program should have the user input a single integer value, and 
+then should print out if that is an even or odd number.
+1/22/2022
 '''
 
 
