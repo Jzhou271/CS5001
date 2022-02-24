d16035e4cd7fba0d45fa5723cf4b3ea516d9842b
Jzhou271 <98428764+Jzhou271@users.noreply.github.com>
2022-02-24 14:11:12 -0800

    Update arithmetic.py

--- week 2/arithmetic.py
+++ week 2/arithmetic.py
@@ -1,7 +1,10 @@
-'''CS5001
-   Lab 2
-   Question 2
-   Jing Zhou
+'''
+In this program you will write a simple arithmetic program. It will 
+take one number as input (either an integer or a floating point number)
+and then a second, and then ask for an operator (+, -, *, /). If a 
+different string is input for an operator display a message saying it
+is an invalid operator (see example below). If the second number is 0 
+and the operator is / the answer should be NaN.
 '''
 
 
