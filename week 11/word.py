e3066e162a627e6982e54180e617c8733a3bfde4
Jzhou271 <98428764+Jzhou271@users.noreply.github.com>
2022-04-13 23:21:38 -0700

    Add files via upload

---
+++ week 11/word.py
@@ -0,0 +1,99 @@
+"""
+CS5001
+Jing Zhou
+Lab 11
+Stacks and Queues
+Using stacks and queues to determine if the input string is a palindrome,
+and to determine if the input string repeats itself a given number of times. 
+"""
+from collections import deque
+
+
+class Word:
+    def __init__(self, input_word):
+        """
+        :param: input_word: str | The string of the word defining this word
+        object
+        """
+        alphabet = "abcdefghijklmnopqrstuvwxyz"
+        self.input_word = input_word
+        for i in input_word:
+            if i not in alphabet:
+                raise ValueError("Invalid input word provided to this Word")
+
+    def __str__(self):
+        """
+        Returns a string that represents this word
+        :param: self | the current word
+        """
+        word = self.input_word
+        return word
+
+    def is_repeat(self, number_repetition):
+        """
+        Returns whether or not this word object repeats itself
+        number_repetition times
+        :param: number_repetition: int | we check if this word repeats at
+        least number_repetition times
+        :return: boolean | if this word repeats number_repetition times
+        """
+        if int(number_repetition) <= 0:
+            raise ValueError("Invalid repetition, number_repetition must"
+                             "greater and equal to 1")
+
+        word_length = len(self.input_word)
+        if word_length % number_repetition != 0:
+            return False
+        stack = deque()
+        base_word_length = word_length // number_repetition
+        base_word = self.input_word[: base_word_length]
+        idx = 0
+
+        for _ in range(number_repetition):
+            stack.append(self.input_word[idx: idx + base_word_length])
+            idx += base_word_length
+        count = 0
+        while stack:
+            if stack.pop() == base_word:
+                count += 1
+        return count == number_repetition
+
+    def is_palindrome(self, number_of_times):
+        """
+        Returns true if this word object is a palindrome.
+        With number_of_times of 1, we check if the word is a palindrome.
+        With number_of_times of 2, we check if the word, when divided in half
+        is a palindrome.
+        With a number_of_times of 3, we check if the word when divided in
+        half twice is a palindrome.
+        :param: number_of_times: int | the number of times to check if this
+        word and its substrings are palindromes
+        :return: boolean | True if the word is a palindrome when subdivided
+        number_of_times times
+        """
+        if int(number_of_times) <= 0:
+            raise ValueError("Invalid repetition, number_repetition must"
+                             "greater and equal to 1")
+
+        # Not sure how the number is divided
+        if number_of_times == 3:
+            number_of_times = 4
+
+        check_word = self.input_word[:len(self.input_word) // number_of_times]
+
+        # Below is to check palindrome using stack
+        stack = deque(list(check_word))
+        for char in check_word:  # String goes from left to right
+            if char != stack.pop():  # Stack goes from right to left, FILO
+                return False
+        return True
+
+
+def main():
+    word = Word("abbaabba")
+    print(word.is_palindrome(1))
+    print(word.is_repeat(2))
+
+
+if __name__ == "__main__":
+    main()
---
+++ week 11/word_tests.py
@@ -0,0 +1,185 @@
+"""
+CS5001
+Jing Zhou
+Lab 11
+    Stacks and Queues -- Testcases
+    Imports from word.py, and tests the Word class by creating objects,
+    calling methods on those objects, and make sure the values of the attributes
+    are what we expect.
+"""
+import unittest
+from word import Word
+import random
+
+
+class WordTests(unittest.TestCase):
+    """
+    This is the unit test class, and it tests all the methods in
+    Word class
+    """
+    def test_init(self):
+        """
+        This test is to check if the constructor works correctly
+        """
+        input_string = Word("abccba")
+        self.assertEqual(input_string.input_word, "abccba")
+
+    def test_str(self):
+        """
+        This test is to check if __str__() works correctly
+        """
+        word = Word("abcddcba")
+        actual = word.__str__()
+        expected = "abcddcba"
+        self.assertEqual(actual, expected)
+ 
+
+    def test_is_repeat_true(self):
+        """
+        This test generates repeated words, and tests is_repeat, always
+        expecting a True answer
+        :return: none
+        """
+        alphabet = "abcdefghijklmnopqrstuvwxyz"
+        for repeats in range(2, 11):
+            for test in range(500):
+                word_length = random.randint(4, 10)
+                input_string = ""
+            for letter in range(word_length):
+                # random append alphabet string character into input_string
+                input_string += random.choice(alphabet)
+
+            # repeat input_string
+            input_string = input_string * repeats
+            word = Word(input_string)
+            actual = word.is_repeat(repeats)
+            self.assertTrue(actual, input_string)
+
+
+    def test_is_repeat_false_odd_length(self):
+        """
+        This test generates repeated words, and tests is_repeat with odd
+        characters, and always expecting a False answer
+        :return: none
+        """
+        alphabet = "abcdefghijklmnopqrstuvwxyz"
+        for repeats in range(2, 11):
+            for test in range(500):
+                word_length = random.randint(4, 10)
+                input_string = ""
+            for letter in range(word_length):
+                # random append alphabet string character into input_string
+                input_string += random.choice(alphabet)
+
+            # repeat input_string
+            input_string = input_string * repeats
+            input_string += random.choice(alphabet)
+            word = Word(input_string)
+            actual = word.is_repeat(repeats)
+            self.assertFalse(actual, input_string)
+
+
+    def test_is_repeat_false_even_length(self):
+        """
+        This test generates repeated words, and tests is_repeat with even
+        characters, and always expecting a False answer
+        :return: none
+        """
+        alphabet = "abcdefghijklmnopqrstuvwxyz"
+        for repeats in range(2, 11):
+            for test in range(500):
+                word_length = random.randint(4, 10)
+                input_string = ""
+            for letter in range(word_length):
+                # random append alphabet string character into input_string
+                input_string += random.choice(alphabet)
+
+            # repeat input_string
+            input_string = input_string * (repeats - 1)
+            letter_to_add = random.choice(alphabet)
+            input_string += letter_to_add * word_length
+            word = Word(input_string)
+            actual = word.is_repeat(repeats)
+            self.assertFalse(actual, input_string)
+
+
+    def test_is_palindrome_true_even(self):
+        """
+        This test generates palindrome words, and tests is_palindrome with even
+        characters, always expecting a True answer
+        :return: none
+        """
+        alphabet = "abcdefghijklmnopqrstuvwxyz"
+        for repeats in range(1, 11):
+            for test in range(10):
+                word_length = random.randint(2, 7)
+                input_string = ""
+                for letter in range(word_length):
+                    # random append alphabet string character into input_string
+                    input_string += random.choice(alphabet)
+
+                # this is the palindrome word
+                input_string = input_string + input_string[::-1]
+                # palindrome repeats n times
+                input_string = input_string * repeats
+                word = Word(input_string)
+                actual = word.is_repeat(repeats)
+                self.assertTrue(actual, input_string)
+
+
+    def test_is_palindrome_true_odd(self):
+        """
+        This test generates palindrome words, and tests is_palindrome with odd
+        characters, always expecting a True answer
+        :return: none
+        """
+        alphabet = "abcdefghijklmnopqrstuvwxyz"
+        for repeats in range(1, 7):
+            for test in range(50):
+                word_length = random.randint(1, 3)
+                # word_length is odd
+                if word_length % 2 != 0:
+                    word_length = word_length
+                else:
+                    word_length = word_length - 1
+                input_string = ""
+                for letter in range(word_length):
+                    # odd random words
+                    input_string += random.choice(alphabet)
+                # palindrome odd
+                input_string = input_string + input_string[::-1]
+                input_string = input_string * (2 * repeats + 1)
+                word = Word(input_string)
+                repeats = 1
+                actual = word.is_palindrome(repeats)
+                self.assertTrue(actual, input_string)
+
+
+    def test_is_palindrome_false(self):
+        """
+        This test generates non-palindrome words, and tests is_palindrome,
+        always expecting a False answer
+        :return: none
+        """
+        alphabet = "abcdefghijklmnopqrstuvwxyz"
+        for repeats in range(1, 11):
+            for test in range(500):
+                word_length = random.randint(2, 7)
+                input_string = ""
+                for letter in range(word_length):
+                    input_string += random.choice(alphabet)
+
+                input_string = input_string + "mn" + input_string[::-1]
+                input_string = input_string * repeats
+                word = Word(input_string)
+                actual = word.is_palindrome(repeats)
+                #print(actual)
+                self.assertFalse(actual, input_string)
+
+
+def main():
+    unittest.main()
+
+
+if __name__ == "__main__":
+    main()
