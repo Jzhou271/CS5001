ce80f20d9e936af6a9ee73295a99d020df1ce30c
Jzhou271 <98428764+Jzhou271@users.noreply.github.com>
2022-04-25 21:33:15 -0700

    Update in_english.py

--- Coding  practice 7/in_english.py
+++ Coding  practice 7/in_english.py
@@ -2,15 +2,15 @@ numEnglish = {0: "zero", 1: "one", 2: "two", 3: "three", 4: "four", 5: "five",
               6: "six", 7: "seven", 8: "eight", 9: "nine", 10: "ten"}
 
 
-def displayEnglishDigits(number):
+def in_english(number):
     if number == 0:
         return ""
     digit = numEnglish[number % 10]
-    return displayEnglishDigits(number // 10) + digit + " "
+    return in_english(number // 10) + digit + " "
 
 
 def main():
-    print(displayEnglishDigits(567))
+    print(in_english(567))
 
 
 if __name__ == "__main__":
